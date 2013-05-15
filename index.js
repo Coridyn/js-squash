2b4f51495b0c701e8e23247b19ea52cd4c1f02ff
Coridyn Fitzgerald-Hood <coridyn.fitzgerald-hood@wotifgroup.com>
2013-05-15 16:45:19 +1000

    - Initial commit, squashes filenames passed on command line.

---
+++ index.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+
+function squash(content){
+	return content.replace(/\n/g, '\\n').replace(/'/g, '\'');
+};
+
+var files = process.argv.slice(2);
+files.forEach(function(file){
+	var content = fs.readFileSync(file, {encoding: 'utf8'});
+	console.log(squash(content));
+});
